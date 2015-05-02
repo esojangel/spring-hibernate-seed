dca4e50031b70b6ba4cece8ccb37567819cff938
jzhang <zhangjunjie5200@163.com>
2015-05-02 22:06:17 +0800

    remove unused import
    Signed-off-by: jzhang <zhangjunjie5200@163.com>

--- spring-hibernate-seed/src/main/java/com/jay/web/controller/HomeController.java
+++ spring-hibernate-seed/src/main/java/com/jay/web/controller/HomeController.java
@@ -5,8 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.jay.util.ContextUtils;
-
 /**
  * 
  * @author Jay Zhang
